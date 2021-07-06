456c309937e68805fde7138977265fbf53bccfe6
Ilya <65218566+igang20@users.noreply.github.com>
2021-07-06 12:01:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+    
+
+
+const start = document.querySelector('.start'), 
+    start1 = document.querySelector('.start1'),     
+    start2 = document.querySelector('.start2'), 
+    start3 = document.querySelector('.start3'); 
+ 
+ 
+start.onclick = function(){ 
+    start.classList.toggle('hide'); 
+};
+
+start1.onclick = function(){ 
+    start1.classList.toggle('hide'); 
+}; 
+ 
+start2.onclick = function(){ 
+    start2.classList.toggle('hide'); 
+}; 
+ 
+start3.onclick = function(){ 
+    start3.classList.toggle('hide'); 
+};
+
+ 
