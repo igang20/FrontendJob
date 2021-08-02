81d17d05d179dfe5a64b504b4445d61c477395a2
Ilya <65218566+igang20@users.noreply.github.com>
2021-08-02 16:56:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -23,4 +23,63 @@ start3.onclick = function(){
     start3.classList.toggle('hide'); 
 };
 
- 
+ /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onClick = function(event) {
+if (!event.target.matches('.searchBarButton1')) {
+
+  var dropdowns = document.getElementsByClassName("dropdownСontent");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
+}
+}
+
+ /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction1() {
+  document.getElementById("myDropdown1").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+if (!event.target.matches('.searchBarButton2')) {
+
+  var dropdowns1 = document.getElementsByClassName("dropdownСontent2")
+  var i;
+  for (i = 0; i < dropdowns1.length; i++) {
+    var openDropdown = dropdowns1[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
+}
+}
+
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+if (!event.target.matches('.searchBarButton3')) {
+
+  var dropdowns1 = document.getElementsByClassName("dropdownСontent3")
+  var i;
+  for (i = 0; i < dropdowns1.length; i++) {
+    var openDropdown = dropdowns1[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
+}
+}
